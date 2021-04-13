af8148eff89b3f92f1cc1dcb365de873eb600f1c
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-04-13 14:10:40 -0500

    Set up template engine.

--- server.js
+++ server.js
@@ -5,12 +5,17 @@ const app = express();
 
 const port = 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
 // use some middleware to serve static assets
 app.use(express.static(path.join(__dirname, './static')));
 
 app.get('/', (req, res) => {
   // respond to a root req w/ the home page html
-  res.sendFile(path.join(__dirname, './static/index.html'));
+  // res.sendFile(path.join(__dirname, './static/index.html'));
+  // - now we're going to use Express' render
+  res.render('pages/index', { pageTitle: 'Welcome' });
 });
 
 app.get('/speakers', (req, res) => {
