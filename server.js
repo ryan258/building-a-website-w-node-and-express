fe3049ee821c3ecdc172c38c70f1b41af82467e4
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-04-13 13:10:56 -0500

    Set up basic server.js w/ express.js to serve static html pages, assets, and images.

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
 const port = 3000
 
+// use some middleware to serve static assets
+app.use(express.static(path.join(__dirname, './static')))
+
 app.get('/', (req, res) => {
-  res.send('Hello from Express.js')
+  // respond to a root req w/ the home page html
+  res.sendFile(path.join(__dirname, './static/index.html'))
+})
+
+app.get('/speakers', (req, res) => {
+  // respond to a root req w/ the home page html
+  res.sendFile(path.join(__dirname, './static/speakers.html'))
+})
+
+app.get('/feedback', (req, res) => {
+  // respond to a root req w/ the home page html
+  res.sendFile(path.join(__dirname, './static/feedback.html'))
 })
 
 app.listen(port, () => {
